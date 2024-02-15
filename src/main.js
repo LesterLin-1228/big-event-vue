7eed995c7716970fc37298f5e83c78d5c5c69c62
LesterLin <newa5812763@gmail.com>
2024-02-15 17:22:54 +0800

    導入pinia持久化插件-persist

--- src/main.js
+++ src/main.js
@@ -3,13 +3,15 @@ import './assets/main.scss'
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import router from '@/router'
 import { createPinia } from 'pinia'
-
+import { createPersistedState } from 'pinia-persistedstate-plugin'
+import router from '@/router'
 import App from './App.vue'
 
 const app = createApp(App);
 const pinia = createPinia();
+const persist = createPersistedState();
+pinia.use(persist);
 app.use(pinia);
 app.use(router);
 app.use(ElementPlus);
